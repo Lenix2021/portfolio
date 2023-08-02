7ca41984b7f95c51d4b42d08ba9a5e00e03fd3f4
Lenix2021 <fianuaaron@gmail.com>
2023-08-02 02:01:37 -0700

    Complete the sections and added some animation and scroll effect

--- script.js
+++ script.js
@@ -57,3 +57,36 @@ circles.forEach(elem=>{
        pointMarked[i].classList.add('marked')  
     }
 })
+
+//mix it up portfolio sectio///////////////////////////////
+var mixer= mixitup('.portfolio-gallery');
+
+
+//active menu ///////////////////////////////
+let menuLi=document.querySelectorAll('header ul li a');
+let section= document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while (--len && window.scrollY + 97 < section[len].offsetTop) {}
+        menuLi.forEach(sec=> sec.classList.remove("active"));
+        menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+//sticky navbar////////////////////////////////////////////////////
+const header=document.querySelector("header")
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY > 50)
+})
+
+//tooggle navbar////////////////////////////////////////////////////
+let menuIcon = document.querySelector("#menu-icon");
+let navalist = document.querySelector(".navlist");
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle("")
+}
