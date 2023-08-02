5f4a76630e0983b69c7d99b1d4e72f3c7049a8a1
Lenix2021 <fianuaaron@gmail.com>
2023-08-02 01:59:06 -0700

    Made alot of changes to the css and worked on the remainig sections

--- script.js
+++ script.js
@@ -37,3 +37,23 @@ words[currentwordindex].style.opacity="1";
 }
  changetext()
  setInterval(changetext,3000)
+
+//  circle skill /////////////////////////////////////////////////////////////////////
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent =Math.floor(dots*marked/100);
+    var points="";
+    var rotate = 360/dots;
+    for (let i = 0; i< dots;  i++) {
+        points +=`  <div class="points" style="--i:${i};--rot:${rotate}deg"></div>`
+    }
+    elem.innerHTML=points;
+
+    const pointMarked = elem.querySelectorAll('.points');
+    for (let i = 0; i<percent; i++) {
+       pointMarked[i].classList.add('marked')  
+    }
+})
